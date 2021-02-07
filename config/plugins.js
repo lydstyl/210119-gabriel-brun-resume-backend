20b9734f1285b30c061e94f00ca1ca9751d70b0c
Gabriel Brun <lydstyl@gmail.com>
2021-02-07 17:45:09 +0100

    try to fix graphql for production doc say ./config not config so we will see

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,16 @@
 module.exports = ({ env }) => ({
+    graphql: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      disabledPlugins: [],
+      disabledExtensions: [],
+      apolloServer: {
+        tracing: false,
+      },
+    },
     upload: {
       provider: 'aws-s3',
       providerOptions: {
